a2a155623d729850ae214c99e8b35e36cd4f64ac
jakehasgithubnow <jacob.murphy@zenjob.com>
2025-03-30 18:09:17 +0200

    Fix next config to ignore ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ this disables build-breaking lint
+  },
+}
 
-export default nextConfig;
+export default nextConfig
