38ab88f94c9119fa04ab01c643071042395d823b
jakehasgithubnow <jacob.murphy@zenjob.com>
2025-04-04 15:46:01 +0200

    Handle null comparisonTable

--- app/tool/[slug]/page.tsx
+++ app/tool/[slug]/page.tsx
@@ -152,10 +152,10 @@ export default async function ToolPage({ params }: any) {
                     Feature
                   </th>
                   <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    {data.comparisonTable.thisToolHeading}
+                    {data.comparisonTable?.thisToolHeading}
                   </th>
                   <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    {data.comparisonTable.competitorHeading}
+                    {data.comparisonTable?.competitorHeading}
                   </th>
                 </tr>
               </thead>
